6c878835049124e4795ad016ce13f0feff331ce2
JoaoCruzFagundes <joao.cruzfagundes@gmail.com>
2019-08-11 12:57:13 -0300

    earl react

---
+++ php/Inicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulario PHP</title>
+</head>
+<body>
+    <div>
+    <form action="google.com">
+  Nome:<br>
+  <input type="text" name="nome" value="Mickey">
+  <br>
+  Sobrenome:<br>
+  <input type="text" name="sobrenome" value="Mouse">
+  <br><br>
+  <input type="submit" value="Enviar">
+</form> 
+<p>Se você clicar no botão "Enviar", os dados do formulário  serão enviados para a página chamada"/action_page.php".</p>
+
+    </div>
+
+
+</body>
+</html>
